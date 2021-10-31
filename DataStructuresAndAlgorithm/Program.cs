959e7f937863522a16ecb19cba9fdd610d2a87d0
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-31 10:17:27 +0530

    adding binary search program

--- DataStructuresAndAlgorithm/Program.cs
+++ DataStructuresAndAlgorithm/Program.cs
@@ -6,7 +6,17 @@ namespace DataStructuresAndAlgorithm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Data Structures and Algorithm");
+            Console.WriteLine("Data Structure!");
+            DataStructure dataStructure = new DataStructure();
+            string[] words = { "decs", "read", "logic", "search" };
+            string data = "search";
+            int result = dataStructure.FindWord(words, data);
+            if (result == -1)
+                Console.WriteLine("Word not present");
+
+            else
+                Console.WriteLine("Word is found at " + result);
         }
     }
+    
 }
